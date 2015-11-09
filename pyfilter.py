ffe1f091a25548f6bde88b1cbffe027c6d4fe407
David Cohen <davelcohen2000@yahoo.com>
2015-11-08 22:36:42 -0500

    Minor changes and added comments.

--- pyfilter.py
+++ pyfilter.py
@@ -21,7 +21,6 @@ def ssqe(sm, s, npts):
 def testGauss(x, y, s, npts):
 	#b = gaussian(39, 10)
 	b = gaussian(75, 15)
-	#ga = filtfilt(b/b.sum(), [1.0], y)
 	ga = filters.convolve1d(y, b/b.sum())
 	plt.plot(x, ga)
 	print "gaerr", ssqe(ga, s, npts)
@@ -59,66 +58,72 @@ if __name__ == '__main__':
 	dataStart = 700
 	# Number of lines to skip at end of file
 	skipEnd = 4000
-	count = 0
-		
-	# Y is a list of the CSV data
+	# Total number of lines in file (including header)
+	totalCount = 0
+
+	# Y is a list of the CSV power data
 	Y = []
-	
+
 	# Need the size of the CSV file (less headers)
 	numLines = 0
-	
+
 	# Open the file
 	inputfile = "20151024_CassA.csv"
 	f = open(inputfile, 'rt')
-	
+
 	try:
 		# Read the file as a CSV, comma-delimited and ' quoted
 		reader = csv.reader(f)          # this id just to get the number of lines in the file
-		count = sum(1 for _ in reader)  # counting lines now
+		totalCount = sum(1 for _ in reader)  # totalCounting lines now
 		f.seek(0)                       # reset the file pointer
 		reader = csv.reader(f)          # reset the csv reader to actually read data
-		dataEnd = count - skipEnd       # the end of the data, or the last line
-		
+		dataEnd = totalCount - skipEnd       # the end of the data, or the last line
+
 		# Skip the first 5 header lines of the file, and whatever number of start and end records desired
 		#for line in range(dataStart):
 		for line in range(HEADERSIZE + dataStart):
 			next(reader)
-			
+
 		for row in reader:	
-			Y.append(float(row[2]))
-			print float(row[2])
-			numLines += 1
+			Y.append(float(row[2])) # read the Y axis data, the power signal
+			print numLines, row[2]
+			numLines += 1           # counnt the number of lines
 			# This is ugly - I throw exception if program goes past end of data
 			if numLines > dataEnd:
-				raise Exception()
-			
+				raise Exception()   # this terminates the loop
+
 	except:
 		pass # something needs to be here
-			
+
 	finally:
 		# Close file when done
 		f.close()
-	
-	s = np.array(Y)
-	npts = numLines
-	end = numLines
-	dt = end/float(npts)
-	nyf = 0.5/dt
-	sigma = 0.5 
-	x = np.arange(end)
-	y = s
-	
+
+	s = np.array(Y)       # convert Y list data to an array
+	npts = numLines       # number of data points in the array - I could eliminate this variable and substitute
+	                      # with numLines, or vice versa
+
+	dt = float(npts)
+	nyf = 0.5/dt          # not sure what this is
+	x = np.arange(npts)   # return evenly spaced values in the interval 0 to end
+			      # this is causing problems as I want the x positions to properly
+			      # represent the data
+	y = s                 # convert Y list data to an array - this is redundant, but required
+
 	# Plot the raw signal
-	plt.xlim([0,end]) # set the xlimit so the data just fits in the graph
+	plt.xlim([0,npts]) # set the xlimit so the data just fits in the graph
+	plt.legend(['Raw Data'], loc='upper center')
 	plt.plot(x,y)
-	
+	plt.savefig("rawdata.png")
+	plt.clf()
+
 	# Plot the filtered signal
 	ga = testGauss(x, y, s, npts)
 	#fl = testButterworth(nyf, x, y, s, npts)
 	#wi = testWiener(x, y, s, npts)
 	#sp = testSpline(x, y, s, npts)
 	#plt.legend(['true','meas','gauss','iir','wie','spl'], loc='upper center')
-	plt.legend(['meas','gauss'], loc='upper center')
-	plt.savefig("signalvsnoise.png")
-	plt.show()
+	plt.legend(['Filtered Data'], loc='upper center')
+	plt.savefig("filteredata.png")
+	#plt.show()
 	plt.clf()
